e7d68341252fbb4a575f6126f2abd3332f428a55
iput2021-Chigira <shunshunshun112311231123@gmail.com>
2023-10-03 21:48:45 +0900

    起動ファイル変更

--- build.sh
+++ build.sh
@@ -2,6 +2,8 @@
 # exit on error
 set -o errexit
 
+/opt/render/project/src/.venv/bin/python -m pip install --upgrade pip
+
 pip install -r requirements.txt
 
 python ./satori/manage.py collectstatic --no-input
